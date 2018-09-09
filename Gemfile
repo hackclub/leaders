3b9a8748ed007149c712b93c889307a6962551bf
MatthewStanciu <mattbstanciu@gmail.com>
2018-09-09 18:44:16 -0400

    added subdomains

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ gem 'sidekiq'
 # Authorization!
 gem 'pundit'
 
+# GitHub REST API wrapper
+gem 'octokit', '~> 4.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/services/subdomains.rb
@@ -0,0 +1,23 @@
+require 'octokit'
+require 'base64'
+require 'yaml'
+
+class Subdomains
+  $client = Octokit::Client.new(:login => 'Orpheus', :password => 'OrpheusPassword') # put actual credentials later
+  contents = $client.contents("hackclub/dns", :path => "hackclub.com.yaml")
+  $decoded_content = Base64.decode64(contents.content)
+  $blob_sha = contents.sha
+
+  def self.append_subdomain(subdomain, host)
+    message = ""
+    new_content = YAML.load($decoded_content + subdomain + ":\n  ttl: 1\n  type: CNAME\n  value: " + host + ".")
+    sorted = Hash[ new_content.sort_by { |key, val| key } ]
+
+    if $decoded_content[subdomain] != nil
+      message = "update subdomain #{subdomain}"
+    else
+      message = "add subdomain #{subdomain}"
+    end
+
+    $client.update_contents("hackclub/dns", "hackclub.com.yaml", message, $blob_sha, sorted.to_yaml)
+  end
