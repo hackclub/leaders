0d5a21ec0b7bbc11cd57b02a772dfab7a7263e8d
Max Wofford <max@maxwofford.com>
2018-09-10 23:18:06 -0700

    Redirect single club users to their club

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def index
     if signed_in?
       @clubs = User.last.clubs_api_record
+      redirect_to club_path(@clubs.first[:slug]) if @clubs.count == 1
     end
   end
 end
