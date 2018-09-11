263a52a46805d3eb725ad13f809ab1ee2d49c04b
Max Wofford <max@maxwofford.com>
2018-09-11 01:50:41 -0700

    Rename type column to record_type

--- app/models/change_request.rb
+++ app/models/change_request.rb
@@ -1,18 +1,24 @@
 class ChangeRequest < ApplicationRecord
-  enum type: %i(CNAME A)
+
+  def self.record_types
+    %i(CNAME A)
+  end
+
+  enum record_type: record_types
 
   belongs_to :user
   belongs_to :subdomain
 
   validates_uniqueness_of :pr_url
-  validates_presence_of :pr_url, :club_id, :user_id, :name, :value, :type
+  validates_presence_of :pr_url, :club_id, :user_id, :name, :value, :record_type
 
   before_create :submit_url
+
 
   private
 
   def submit_pull_request
-    pr_url = SubdomainService.append_subdomain(name, type, value)
+    pr_url = SubdomainService.append_subdomain(name, record_type, value)
     self.pr_url = pr_url
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_11_042246) do
+ActiveRecord::Schema.define(version: 2018_09_11_084814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2018_09_11_042246) do
     t.bigint "user_id"
     t.bigint "subdomain_id"
     t.string "value"
-    t.integer "type"
+    t.integer "record_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["subdomain_id"], name: "index_change_requests_on_subdomain_id"
