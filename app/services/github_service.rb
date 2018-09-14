fee26edccdadcb570f1816870634463f07e07d17
Max Wofford <max@maxwofford.com>
2018-09-14 13:22:57 -0700

    Fix up GithubService
    Went through a bunch of the resources & made small fixes anytime I saw GithubService misbehaving

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -7,33 +7,19 @@ class GithubService
   REPO = "hackclub/dns"
   FILE = "hackclub.com.yaml"
 
-  # def self.submit_pr(subdomain)
-  #   old_file = client.contents(REPO, path: FILE)
-  #   blob_sha = old_file.sha
-  #
-  #   head_sha = client.ref(REPO, "heads/master")[:object][:sha]
-  #   branches = client.branches(REPO).pluck(:name)
-  #
-  #   new_branch_name = branch_name(branches, subdomain)
-  #   new_ref_name    = "heads/#{new_branch_name}"
-  #
-  #   client.create_ref(REPO, new_ref_name, head_sha)
-  #
-  #   new_content = updated_content(old_file.content, subdomain, type, value)
-  #
-  #   message = "#{type} record for #{subdomain}.hackclub.com"
-  #
-  #   client.update_contents(REPO, FILE, message, blob_sha, new_content, branch: new_branch_name)
-  #
-  #   pull_request = client.create_pull_request(REPO, "master", new_branch_name, message)
-  #   pull_request
-  # end
-
   def self.generate_and_submit_pr(subdomain)
     dns_data = get_dns_records
-    dns_data[subdomain[:name]] = []
-    subdomain.dns_records.each do |dns_record|
-      add_dns_record(dns_data, subdomain, dns_record[:record_type], dns_record[:value])
+
+    if subdomain.changes
+      old_name = subdomain&.changes[:name]&.first
+      dns_data.delete old_name
+    end
+
+    if subdomain.dns_records.any?
+      dns_data[subdomain[:name]] = []
+      subdomain.dns_records.each do |dns_record|
+        add_dns_record(dns_data, subdomain, dns_record[:record_type], dns_record[:value])
+      end
     end
 
     create_pr(dns_data.to_yaml, "Update #{subdomain[:name]}.hackclub.com", subdomain[:name])
@@ -43,14 +29,23 @@ class GithubService
     pr_gh = client.pull_request(REPO, pr[:number])
     blob_sha = client.contents(REPO, path: FILE, ref: pr_gh[:head][:ref]).sha
     head_sha = pr_gh[:head][:sha]
+    subdomain = pr.subdomain
 
     dns_data = get_dns_records(pr_gh[:head][:ref])
-    dns_data[pr.subdomain[:name]] = []
-    pr.subdomain.dns_records.each do |dns_record|
-      add_dns_record(dns_data, pr.subdomain, dns_record[:record_type], dns_record[:value])
+
+    if subdomain.changes
+      old_name = subdomain&.changes[:name]&.first
+      dns_data.delete old_name
     end
 
-    client.update_contents(REPO, FILE, "Update #{pr.subdomain[:name]}.hackclub.com", blob_sha, dns_data.to_yaml, branch: pr_gh[:head][:ref])
+    if subdomain.dns_records.any?
+      dns_data[subdomain[:name]] = []
+      subdomain.dns_records.each do |dns_record|
+        add_dns_record(dns_data, subdomain, dns_record[:record_type], dns_record[:value])
+      end
+    end
+
+    client.update_contents(REPO, FILE, "Update #{subdomain[:name]}.hackclub.com", blob_sha, dns_data.to_yaml, branch: pr_gh[:head][:ref])
   end
 
   def self.create_pr(content, message, subdomain_name)
@@ -60,7 +55,6 @@ class GithubService
 
     new_branch_name = branch_name(branches, subdomain_name)
     new_ref_name    = "heads/#{new_branch_name}"
-
     client.create_ref(REPO, new_ref_name, head_sha)
 
     client.update_contents(REPO, FILE, message, blob_sha, content, branch: new_branch_name)
@@ -115,5 +109,7 @@ class GithubService
 
   def self.client
     @client ||= Octokit::Client.new(access_token: Rails.application.credentials[:github_token])
+    @client.auto_paginate = true
+    return @client
   end
 end
