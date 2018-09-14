e8de8fb67bb6a22b5ccbb596b393fdb1807927d2
Max Wofford <max@maxwofford.com>
2018-09-14 13:21:46 -0700

    Fix broken links to subdomain page

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,5 +1,4 @@
 class ClubsController < ApplicationController
-  before_action :set_club, only: [:show, :edit]
 
   # GET /clubs
   def index
@@ -9,13 +8,8 @@ class ClubsController < ApplicationController
 
   # GET /clubs/1
   def show
-    # authorize @club
-  end
-
-  private
-
-  def set_club
     @club = Club.find_by(slug: params[:slug])
-    @subdomain = Subdomain.find_or_initialize_by(club_id: @club[:id])
+    @subdomains = @club.subdomains
+    # authorize @club
   end
 end
