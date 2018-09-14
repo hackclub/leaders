7187adc206565382624cdcdc4f7d8dc6df04ef0f
Max Wofford <max@maxwofford.com>
2018-09-14 05:24:49 -0700

    Add support for deleting records

--- app/models/dns_record.rb
+++ app/models/dns_record.rb
@@ -10,11 +10,13 @@ class DnsRecord < ApplicationRecord
   has_one :change_request
 
   validates_presence_of :user_id, :value, :record_type
-  validates_uniqueness_of :record_type, scope: :subdomain
+  validates_uniqueness_of :record_type, scope: [:subdomain, :deleted_at], if: -> { deleted_at.nil? }
+
+  default_scope { where(deleted_at: nil) }
+  scope :include_deleted, -> { unscope(:where) }
 
   after_save do |dns_record|
     # Get the branch's open PR or create a new one
-
     subdomain.pull_requests.active.each(&:update_github_status)
     active_pr = subdomain.pull_requests.order(:created_at).active.last
 
