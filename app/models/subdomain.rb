c613fb38639dd88ce2aead6c327336be824e8533
Max Wofford <max@maxwofford.com>
2018-09-14 06:00:12 -0700

    Update GitHub when changing subdomain name

--- app/models/subdomain.rb
+++ app/models/subdomain.rb
@@ -1,11 +1,13 @@
 class Subdomain < ApplicationRecord
   validates_uniqueness_of :club_id, :name
   validates_presence_of :club_id, :name
-  validate :vacant_subdomain_name, on: :create
+  validate :vacant_subdomain_name, if: :name_changed?
 
   has_many :dns_records
   has_many :pull_requests
   belongs_to :club
+
+  before_save :create_pr, if: :name_changed?
 
   def slug
     name
@@ -24,5 +26,16 @@ class Subdomain < ApplicationRecord
   def vacant_subdomain_name
     return if GithubService.subdomain_available? name
     errors.add(:name, 'already taken')
+  end
+
+  def create_pr
+    pull_requests.active.each(&:update_github_status)
+    active_pr = pull_requests.order(:created_at).active.last
+
+    if active_pr
+      active_pr.update_github_changes(self)
+    else
+      PullRequest.create!(subdomain: self)
+    end
   end
 end
