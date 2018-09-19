e87396c4d02853e9f758b4080bcf49f8f8e84801
Lachlan Campbell <lachlan@hackclub.com>
2018-09-19 00:35:47 -0400

    Create Gravatar helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
+require 'digest/md5'
+
 module UsersHelper
+  def gravatar_url(email, size = 64)
+    hex = Digest::MD5.hexdigest(email.downcase.strip)
+    "https://gravatar.com/avatar/#{hex}?s=#{size}"
+  end
+
+  def gravatar_for(email, size, options = {})
+    image_tag gravatar_url(email, size), options
+  end
 end
