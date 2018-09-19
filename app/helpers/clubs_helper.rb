4cc9185eb333ddbef29d8bce3dc1d97c68c1f249
Lachlan Campbell <lachlan@hackclub.com>
2018-09-19 16:40:14 -0400

    Use map as page background

--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -1,5 +1,5 @@
 module ClubsHelper
   def map_url(lat, lng, options = {})
-    "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/#{lng},#{lat},4.75,0,24/1024x256@2x?access_token=pk.eyJ1IjoiaGFja2NsdWIiLCJhIjoiY2pscGI1eGdhMGRyNzN3bnZvbGY5NDBvZSJ9.Zm4Zduj94TrgU8h890M7gA"
+    "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/#{lng},#{lat},5,0,24/1280x1280@2x?access_token=pk.eyJ1IjoiaGFja2NsdWIiLCJhIjoiY2pscGI1eGdhMGRyNzN3bnZvbGY5NDBvZSJ9.Zm4Zduj94TrgU8h890M7gA"
   end
 end
