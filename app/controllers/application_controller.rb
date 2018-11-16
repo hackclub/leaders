ca2fcce4f9dabca6a12b364bf8b674f280ab826d
Lachlan Campbell <lachlan@hackclub.com>
2018-11-16 14:18:16 -0500

    Add another…

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,11 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     flash[:error] = 'You are not authorized to perform this action.'
-    redirect_to(root_path)
+    redirect_to root_path
   end
 
   def api_auth_invalid
     flash[:error] = 'Your authorization is invalid or expired.'
-    redirect_to(auth_users_path)
+    redirect_to sign_in_users_path
   end
 end
