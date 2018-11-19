a00b1bd98a079ea2d3056cc2f1996ef748f01f6d
Lachlan Campbell <lachlan@hackclub.com>
2018-11-19 14:13:32 -0500

    Add validation to files

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,18 @@ class Post < ApplicationRecord
   validates :name, length: { minimum: 6, too_short: '– write a more descriptive title' }
   
   has_one_attached :file
+
+  validate :file_validation
+
+  def file_validation
+    if file.attached?
+      if file.blob.byte_size > 5000000
+        file.purge
+        errors[:file] << '– too large (5MB limit)'
+      elsif !file.blob.content_type.starts_with?('image/')
+        file.purge
+        errors[:file] << '— only images are supported'
+      end
+    end
+  end
 end
