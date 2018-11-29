58e99c9de0aa338a9da2af7ff768200e5735a40c
Max Wofford <max@maxwofford.com>
2018-11-29 14:11:17 -0800

    Update meeting policy to protect edit & update routes

--- app/policies/meeting_policy.rb
+++ app/policies/meeting_policy.rb
@@ -2,4 +2,12 @@ class MeetingPolicy < ApplicationPolicy
   def show?
     user.admin? || meeting.club.api_record['new_leaders'].map{ |l| l['email'] }.include?(user.email)
   end
+
+  def edit?
+    user.admin? || meeting.club.api_record['new_leaders'].map{ |l| l['email'] }.include?(user.email)
+  end
+
+  def update?
+    user.admin? || meeting.club.api_record['new_leaders'].map{ |l| l['email'] }.include?(user.email)
+  end
 end
