c435c8f20d55f394dac4db8c5a7e8790f4011de9
Max Wofford <max@maxwofford.com>
2018-12-08 21:53:21 -0800

    Attempt to fix map_url always returning same image

--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -4,8 +4,8 @@ module ClubsHelper
   end
 
   def map_url(club, options = {})
-    lat = club.api_record[:high_school_latitude] || 41.838796
-    lng = club.api_record[:high_school_longitude] || -83.200725
+    lat = club.api_record['high_school_latitude'] || 41.838796
+    lng = club.api_record['high_school_longitude'] || -83.200725
     # default lat & lng are a beautiful location I found that is used when arguments are null
     "https://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial/#{lat},#{lng}/11/?mapSize=2000,1500&format=png&key=AssBchuxLMpaS6MmACdfDyLpD4X7_T2SZ34cC_KBcWlPU6iZCsWgv0tTbw5Coehm"
   end
