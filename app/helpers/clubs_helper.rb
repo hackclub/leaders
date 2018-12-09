303d5b6489a479baa436d979711cc4eacf75365e
Max Wofford <max@maxwofford.com>
2018-12-08 21:51:32 -0800

    Simplify map_url helper

--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -3,9 +3,9 @@ module ClubsHelper
     name.to_s.downcase.remove(' ').remove(/\W+/).remove('hackclub').remove('high').remove('school')
   end
 
-  def map_url(lat=nil, lng=nil, options = {})
-    lat ||= 41.838796
-    lng ||= -83.200725
+  def map_url(club, options = {})
+    lat = club.api_record[:high_school_latitude] || 41.838796
+    lng = club.api_record[:high_school_longitude] || -83.200725
     # default lat & lng are a beautiful location I found that is used when arguments are null
     "https://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial/#{lat},#{lng}/11/?mapSize=2000,1500&format=png&key=AssBchuxLMpaS6MmACdfDyLpD4X7_T2SZ34cC_KBcWlPU6iZCsWgv0tTbw5Coehm"
   end
