f6a1620d38153a3e52a32d11bac41536f97c19d9
Max Wofford <max@maxwofford.com>
2018-12-08 21:48:54 -0800

    Switch from mapbox to virtualearth

--- app/helpers/clubs_helper.rb
+++ app/helpers/clubs_helper.rb
@@ -4,9 +4,9 @@ module ClubsHelper
   end
 
   def map_url(lat=nil, lng=nil, options = {})
-    lat ||= -65
-    lng ||= 92.8
+    lat ||= 41.838796
+    lng ||= -83.200725
     # default lat & lng are a beautiful location I found that is used when arguments are null
-    "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/#{lng},#{lat},5,0,24/1280x1280@2x?access_token=pk.eyJ1IjoiaGFja2NsdWIiLCJhIjoiY2pscGI1eGdhMGRyNzN3bnZvbGY5NDBvZSJ9.Zm4Zduj94TrgU8h890M7gA"
+    "https://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial/#{lat},#{lng}/11/?mapSize=2000,1500&format=png&key=AssBchuxLMpaS6MmACdfDyLpD4X7_T2SZ34cC_KBcWlPU6iZCsWgv0tTbw5Coehm"
   end
 end
