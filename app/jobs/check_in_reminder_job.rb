da926e4dba163fa9ec0c6b4ad7f4743a052801c1
Max Wofford <max@maxwofford.com>
2019-02-03 19:34:04 -0800

    Attempt to fix check-in reminder job

--- app/jobs/check_in_reminder_job.rb
+++ app/jobs/check_in_reminder_job.rb
@@ -5,7 +5,9 @@ class CheckInReminderJob < ApplicationJob
     self.class.set(wait: 1.day).perform_later(true) if repeat
 
     Club.select{ |club| club.meeting_day == Date.today.wday }.each do |club|
-      club.users.each{ |user| notify(user, club) if (user.email_on_check_in?) }
+      # clubs can have nil leaders, so we need to remove them from our list
+      users = club.users.compact
+      users.each{ |user| notify(user, club) if (user.email_on_check_in?) }
     end
   end
 
