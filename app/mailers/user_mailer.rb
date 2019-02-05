3fb70bf3b646400e770012a185daf33ea4ad3ec6
Max Wofford <max@maxwofford.com>
2019-02-05 15:01:20 -0800

    Add reply-to mailing list for check-ins

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,9 @@ class UserMailer < ApplicationMailer
 
     clubs.each do |club|
       @club = club
-      mail to: email, subject: "Check-in w/ #{@club.name} Hack Club on #{Date.today.to_s}"
+      mail to: email,
+           reply_to: 'checkin@hackclub.com',
+           subject: "Check-in w/ #{@club.name} Hack Club on #{Date.today.to_s}"
     end
   end
 end
